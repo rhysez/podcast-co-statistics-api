7965f16334ef1d251ccd189e076aefd53af1b67f
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-01-22 20:28:13 +0000

    feat: add create-downloads-table migration

---
+++ database/migrations/2026_01_22_202530_create-downloads-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('event_id')->unique();
+            $table->uuid('podcast_id')->index();
+            $table->uuid('episode_id')->index();
+            $table->timestamp('occurred_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('downloads');
+    }
+};
