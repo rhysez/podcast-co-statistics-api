7821030015cadd42f6c4ba9f162c32aa5029fb53
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-01-22 22:01:42 +0000

    chore: opt /api/webhook route out of csrf checks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: ['api/webhook']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
