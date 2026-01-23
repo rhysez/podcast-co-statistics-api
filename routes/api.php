79fc96426618f5fdccf96cac7bc8d3d2aba62f7f
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-01-23 17:35:33 +0000

    feat: register endpoint /episodes/{id}/stats to DownloadController->index

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\DownloadController;
 use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/webhook', [EventController::class, 'webhookHandler']);
-
+Route::get('/episodes/{id}/stats', [DownloadController::class, 'index']);
