8c7f47bf7d25d624cb6cf90dee62f5f4a6675eb2
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-01-27 21:06:33 +0000

    feat: add test to check that the job throws contraint violation in the case that the event_id is a duplicate

--- tests/Feature/ProcessDownloadTest.php
+++ tests/Feature/ProcessDownloadTest.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Jobs\ProcessDownload;
+use App\Models\Download;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 uses(RefreshDatabase::class);
-
-
 
 test('download job creates a download record', function () {
     $payload = [
@@ -28,4 +28,28 @@ test('download job creates a download record', function () {
         'podcast_id' => $payload['data']['podcast_id'],
         'episode_id' => $payload['data']['episode_id'],
     ]);
+});
+
+test('download job does not create duplicate download for same event_id', function () {
+    $eventId = Str::uuid()->toString();
+
+    Download::factory()->create([
+        'event_id' => $eventId,
+    ]);
+
+    $payload = [
+        'event_id' => $eventId,
+        'occurred_at' => now(),
+        'data' => [
+            'podcast_id' => Str::uuid()->toString(),
+            'episode_id' => Str::uuid()->toString(),
+        ],
+    ];
+
+    // I'm not explicitly handling the exception thrown by the job here because duplicates are prevented
+    // in the webhook controller, but if this job is dispatched from other areas of the API then I think we
+    // may want to handle the duplicates in the job. Open to discussion on this, I think the main issue with
+    // handling the exception in the job is that it doesn't provide any response to the user's request.
+    $this->expectException(QueryException::class);
+    (new ProcessDownload($payload))->handle();
 });
